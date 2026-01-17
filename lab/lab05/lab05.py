15bf5c3a0b4ff52ac4d7b1a3d785012933ec7f15
phainon2367 <robertyu2024@163.com>
2026-01-17 15:55:16 +0800

    updated the last 2 questions' solutions in lab05

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -347,7 +347,11 @@ def build_successors_table(tokens):
     for word in tokens:
         if prev not in table:
             "*** YOUR CODE HERE ***"
+            table[prev] = [word]
+            prev = word
+            continue
         "*** YOUR CODE HERE ***"
+        table[prev].append(word)
         prev = word
     return table
 
@@ -365,6 +369,8 @@ def construct_sent(word, table):
     result = ''
     while word not in ['.', '!', '?']:
         "*** YOUR CODE HERE ***"
+        result += f" {word}"
+        word = random.choice(table[word])
     return result.strip() + word
 
 def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
